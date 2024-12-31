220cf22dbf0e75b80b042844c71301b9185e10e8
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2025-01-01 03:17:20 +0500

    fix fillters

--- src/App/pages/ProductListing.jsx
+++ src/App/pages/ProductListing.jsx
@@ -13,11 +13,136 @@ const ProductListingPage = () => {
     priceRange: [0, 1000],
     brand: "",
     rating: 0,
+    cities: [],
+    sellerTypes: [],
   });
 
   const [isFilterModalOpen, setIsFilterModalOpen] = useState(false); // For mobile sidebar modal
 
-  const { products: SearchedProduct, searchQuery, error } = location.state || {
+  const [citySuggestions, setCitySuggestions] = useState([]);
+  const [searchCity, setSearchCity] = useState("");
+  const [sellerTypes] = useState([
+    "Exporter",
+    "Wholesaler",
+    "Retailer",
+    "Manufacturer",
+  ]);
+
+  const cities = [
+    "Karachi",
+    "Lahore",
+    "Islamabad",
+    "Rawalpindi",
+    "Peshawar",
+    "Quetta",
+    "Faisalabad",
+    "Multan",
+    "Hyderabad",
+    "Sialkot",
+    "Bahawalpur",
+    "Gujranwala",
+    "Sukkur",
+    "Abbottabad",
+    "Mardan",
+    "Sheikhupura",
+    "Gujrat",
+    "Sargodha",
+    "Jhelum",
+    "Okara",
+    "Rahim Yar Khan",
+    "Dera Ghazi Khan",
+    "Chiniot",
+    "Mansehra",
+    "Mirpur",
+    "Khairpur",
+    "Kasur",
+    "Nawabshah",
+    "Larkana",
+    "Bannu",
+    "Chaman",
+    "Swat",
+    "Turbat",
+    "Dera Ismail Khan",
+    "Charsadda",
+    "Muzaffargarh",
+    "Mingora",
+    "Jamshoro",
+    "Ghotki",
+    "Hafizabad",
+    "Attock",
+    "Vehari",
+    "Jhang",
+    "Tando Allahyar",
+    "Tando Adam",
+    "Sahiwal",
+    "Kotli",
+    "Hangu",
+    "Kohat",
+    "Mandi Bahauddin",
+    "Nowshera",
+    "Bhakkar",
+    "Haripur",
+    "Khushab",
+    "Shikarpur",
+    "Jacobabad",
+    "Kandhkot",
+    "Bhalwal",
+    "Kamoke",
+    "Daska",
+    "Chakwal",
+    "Pakpattan",
+    "Hub",
+    "Badin",
+    "Dadu",
+    "Kharian",
+    "Gwadar",
+    "Umerkot",
+    "Zhob",
+    "Chitral",
+    "Toba Tek Singh",
+    "Samundri",
+    "Kashmore",
+    "Jaranwala",
+    "Khuzdar",
+    "Sanghar",
+    "Matiari",
+    "Burewala",
+    "Narowal",
+    "Mirpurkhas",
+    "Pasrur",
+    "Kamra",
+    "Gakhar Mandi",
+    "Shujaabad",
+    "Kot Adu",
+    "Tank",
+    "Layyah",
+    "Hub Chowki",
+    "Hasilpur",
+    "Tando Muhammad Khan",
+    "Arifwala",
+    "Kabirwala",
+    "Chunian",
+    "Pattoki",
+    "Haveli Lakha",
+    "Murree",
+    "Fort Abbas",
+    "Lodhran",
+    "Depalpur",
+    "Darya Khan",
+    "Bhawana",
+    "Kallar Kahar",
+    "Pir Mahal",
+    "Kot Radha Kishan",
+    "Rohri",
+    "Talagang",
+    "Taunsa",
+  ];
+
+  const {
+    products: SearchedProduct,
+    searchQuery,
+    error,
+  } = location.state || {
     products: null,
     searchQuery: "",
     error: "",
@@ -79,6 +204,18 @@ const ProductListingPage = () => {
         );
       }
 
+      if (filters.cities.length > 0) {
+        filtered = filtered.filter((product) =>
+          filters.cities.includes(product.city)
+        );
+      }
+
+      if (filters.sellerTypes.length > 0) {
+        filtered = filtered.filter((product) =>
+          filters.sellerTypes.includes(product.sellerType)
+        );
+      }
+
       setFilteredProducts(filtered);
     };
 
@@ -95,28 +232,59 @@ const ProductListingPage = () => {
       priceRange: [0, 1000],
       brand: "",
       rating: 0,
+      cities: [],
+      sellerTypes: [],
+    });
+  };
+
+  const handleCitySearch = (e) => {
+    const query = e.target.value;
+    setSearchCity(query);
+
+    if (query) {
+      const suggestions = cities.filter((city) =>
+        city.toLowerCase().includes(query.toLowerCase())
+      );
+      setCitySuggestions(suggestions);
+    } else {
+      setCitySuggestions([]);
+    }
+  };
+
+  const handleCitySelect = (city) => {
+    if (!filters.cities.includes(city)) {
+      setFilters({ ...filters, cities: [...filters.cities, city] });
+    }
+    setSearchCity("");
+    setCitySuggestions([]);
+  };
+
+  const handleChipRemove = (key, value) => {
+    setFilters({
+      ...filters,
+      [key]: filters[key].filter((item) => item !== value),
     });
   };
 
   return (
     <>
-    <div className="py-3 p-1 md:px-4 ">
-
-      <Breadcrumb
-        items={[
-          { name: "Home", link: "/" },
-          { name: "Products", link: "/products" },
-        ]}
+      <div className="py-3 p-1 md:px-4">
+        <Breadcrumb
+          items={[
+            { name: "Home", link: "/" },
+            { name: "Products", link: "/products" },
+          ]}
         />
-        </div>
+      </div>
+
       <div className="flex flex-col md:flex-row gap-4 px-4 md:px-8 lg:px-16 py-4">
-        {/* Filters for Mobile */}
-        <button
+      <button
           className="md:hidden bg-blue-500 text-white py-2 px-4 rounded-lg shadow-md mb-4"
           onClick={() => setIsFilterModalOpen(true)}
         >
           Show Filters
         </button>
+
 
         {/* Filter Sidebar Modal */}
         {isFilterModalOpen && (
@@ -131,160 +299,158 @@ const ProductListingPage = () => {
                   Close
                 </button>
               </div>
-              <aside>
-                {/* Filters */}
-                <div className="mb-6">
-                  <h4 className="font-medium mb-2">Categories</h4>
-                  <ul className="space-y-2">
-                    {categories?.map((category, index) => (
-                      <li key={index}>
-                        <label className="inline-flex items-center">
-                          <input
-                            type="radio"
-                            name="category"
-                            value={category}
-                            className="form-radio text-blue-500"
-                            onChange={() =>
-                              handleFilterChange("category", category.slug)
-                            }
-                          />
-                          <span className="ml-2 capitalize">{category.slug}</span>
-                        </label>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mb-6">
-                  <h4 className="font-medium mb-2">Price Range</h4>
-                  <input
-                    type="range"
-                    min="0"
-                    max="1000"
-                    step="10"
-                    value={filters.priceRange[1]}
-                    className="w-full"
-                    onChange={(e) =>
-                      handleFilterChange("priceRange", [0, parseInt(e.target.value)])
-                    }
-                  />
-                  <div className="flex justify-between text-sm">
-                    <span>$0</span>
-                    <span>${filters.priceRange[1]}</span>
-                  </div>
-                </div>
-                <div>
-                  <h4 className="font-medium mb-2">Ratings</h4>
-                  <ul className="space-y-2">
-                    {[5, 4, 3, 2, 1].map((rating) => (
-                      <li key={rating}>
-                        <label className="inline-flex items-center">
-                          <input
-                            type="radio"
-                            name="rating"
-                            className="form-radio text-yellow-500"
-                            onChange={() => handleFilterChange("rating", rating)}
-                          />
-                          <span className="ml-2">{rating} Stars & Up</span>
-                        </label>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                {/* Submit and Reset buttons */}
-                <div className="flex justify-between mt-6">
-                  <button
-                    className="bg-blue-500 text-white py-2 px-4 rounded-lg"
-                    onClick={() => setIsFilterModalOpen(false)}
+              <aside className="md:w-1/4 bg-gray-100 p-4 rounded-lg shadow-md">
+          {/* <h3 className="text-lg font-semibold mb-4">Filters</h3> */}
+
+          {/* City Filter */}
+          <div className="mb-6">
+            <h4 className="font-medium mb-2">Cities</h4>
+            <input
+              type="text"
+              placeholder="Search by city"
+              value={searchCity}
+              onChange={handleCitySearch}
+              className="w-full px-4 py-2 border rounded-lg"
+            />
+            {citySuggestions.length > 0 && (
+              <ul className="absolute bg-white border mt-1  max-h-40 overflow-auto rounded-lg shadow-lg z-10">
+                {citySuggestions.map((city, index) => (
+                  <li
+                    key={index}
+                    onClick={() => handleCitySelect(city)}
+                    className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
                   >
-                    Apply Filters
-                  </button>
-                  <button
-                    className="bg-gray-300 text-black py-2 px-4 rounded-lg"
-                    onClick={handleResetFilters}
-                  >
-                    Reset Filters
-                  </button>
-                </div>
-              </aside>
+                    {city}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          {/* Seller Type Filter */}
+          <div className="mb-6">
+            <h4 className="font-medium mb-2">Seller Types</h4>
+            <div className="flex flex-wrap gap-2">
+              {sellerTypes.map((type, index) => (
+                <button
+                  key={index}
+                  onClick={() =>
+                    handleFilterChange(
+                      "sellerTypes",
+                      filters.sellerTypes.includes(type)
+                        ? filters.sellerTypes.filter((item) => item !== type)
+                        : [...filters.sellerTypes, type]
+                    )
+                  }
+                  className={`px-3 py-1 rounded-lg ${
+                    filters.sellerTypes.includes(type)
+                      ? "bg-blue-500 text-white"
+                      : "bg-gray-200"
+                  }`}
+                >
+                  {type}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          {/* Reset Filters */}
+          <button
+            onClick={handleResetFilters}
+            className="w-full px-4 py-2 bg-red-500 text-white rounded-lg"
+          >
+            Reset Filters
+          </button> 
+        </aside>
             </div>
           </div>
         )}
 
-        {/* Filter Sidebar for Larger Screens */}
-        <aside className="hidden md:block w-1/4 bg-gray-100 p-4 rounded-lg shadow-md">
+        <aside className="md:w-1/4 bg-gray-100 p-4 rounded-lg shadow-md hidden sm:block">
           <h3 className="text-lg font-semibold mb-4">Filters</h3>
-          {/* Same Filters as Modal */}
-          {/* Categories */}
+
+          {/* City Filter */}
           <div className="mb-6">
-            <h4 className="font-medium mb-2">Categories</h4>
-            <ul className="space-y-2">
-              {categories?.map((category, index) => (
-                <li key={index}>
-                  <label className="inline-flex items-center">
-                    <input
-                      type="radio"
-                      name="category"
-                      value={category}
-                      className="form-radio text-blue-500"
-                      onChange={() =>
-                        handleFilterChange("category", category.slug)
-                      }
-                    />
-                    <span className="ml-2 capitalize">{category.slug}</span>
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
-          {/* Price */}
-          <div className="mb-6">
-            <h4 className="font-medium mb-2">Price Range</h4>
+            <h4 className="font-medium mb-2">Cities</h4>
             <input
-              type="range"
-              min="0"
-              max="1000"
-              step="10"
-              value={filters.priceRange[1]}
-              className="w-full"
-              onChange={(e) =>
-                handleFilterChange("priceRange", [0, parseInt(e.target.value)])
-              }
+              type="text"
+              placeholder="Search by city"
+              value={searchCity}
+              onChange={handleCitySearch}
+              className="w-full px-4 py-2 border rounded-lg"
             />
-            <div className="flex justify-between text-sm">
-              <span>$0</span>
-              <span>${filters.priceRange[1]}</span>
+            {citySuggestions.length > 0 && (
+              <ul className="absolute bg-white border mt-1  max-h-40 overflow-auto rounded-lg shadow-lg z-10">
+                {citySuggestions.map((city, index) => (
+                  <li
+                    key={index}
+                    onClick={() => handleCitySelect(city)}
+                    className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+                  >
+                    {city}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          {/* Seller Type Filter */}
+          <div className="mb-6">
+            <h4 className="font-medium mb-2">Seller Types</h4>
+            <div className="flex flex-wrap ga p-2">
+              {sellerTypes.map((type, index) => (
+                <button
+                  key={index}
+                  onClick={() =>
+                    handleFilterChange(
+                      "sellerTypes",
+                      filters.sellerTypes.includes(type)
+                        ? filters.sellerTypes.filter((item) => item !== type)
+                        : [...filters.sellerTypes, type]
+                    )
+                  }
+                  className={`px-3 py-1 rounded-lg ${
+                    filters.sellerTypes.includes(type)
+                      ? "bg-blue-500 text-white"
+                      : "bg-gray-200"
+                  }`}
+                >
+                  {type}
+                </button>
+              ))}
             </div>
           </div>
-          {/* Ratings */}
-          <div>
-            <h4 className="font-medium mb-2">Ratings</h4>
-            <ul className="space-y-2">
-              {[5, 4, 3, 2, 1].map((rating) => (
-                <li key={rating}>
-                  <label className="inline-flex items-center">
-                    <input
-                      type="radio"
-                      name="rating"
-                      className="form-radio text-yellow-500"
-                      onChange={() => handleFilterChange("rating", rating)}
-                    />
-                    <span className="ml-2">{rating} Stars & Up</span>
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
+
+          {/* Reset Filters */}
+          <button
+            onClick={handleResetFilters}
+            className="w-full px-4 py-2 bg-red-500 text-white rounded-lg"
+          >
+            Reset Filters
+          </button> 
         </aside>
 
-        {/* Main Content */}
-        <div className="flex-1">
-          <h2 className="text-lg md:text-xl mb-4">
-            {searchQuery
-              ? `Search Results for "${searchQuery}" (${filteredProducts.length})`
-              : `Products (${filteredProducts.length})`}
-          </h2>
+        <div className="md:w-3/4">
+          <div className="mb-2">
+            <div className="flex justify-between items-center flex-row">
+              <h2 className="text-md md:text-lg mb-2">
+                {searchQuery
+                  ? `Search Results for "${searchQuery}" (${filteredProducts.length})`
+                  : `Products (${filteredProducts.length})`}
+              </h2>
+
+              <h2 className="text-md md:text-lg mb-2">
+                {filters.cities.length > 0
+                  ? `Products in "${filters.cities.join(", ")}" (${
+                      filteredProducts.length
+                    })`
+                  : `Products (${filteredProducts.length})`}
+              </h2>
+            </div>
+          </div>
+
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
-            {filteredProducts?.map((product) => (
+            {filteredProducts.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
           </div>
